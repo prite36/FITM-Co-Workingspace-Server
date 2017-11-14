16dc1cc9630b69c5c43ae8dcbdf306c0b137918a
prite36 <spritelemon36@hotmail.com>
2017-11-14 16:34:38 +0700

    fix post back

--- messenger/receive.js
+++ messenger/receive.js
@@ -57,7 +57,7 @@ const receivedPostback = (event) => {
   var senderID = event.sender.id
   var recipientID = event.recipient.id
   var timeOfPostback = event.timestamp
-  var {type, payload} = event.postback.payload
+  var {type, payload} = JSON.parse(event.postback.payload)
   console.log('Received postback for user %d and page %d with payload %s ' + 'at %d', senderID, recipientID, payload, timeOfPostback)
   if (type === 'GET_STARTED') {
     firebaseDB.checkUserGetStart(senderID)
