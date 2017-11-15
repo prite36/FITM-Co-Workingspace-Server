1d6ddcfa98ef1bb3891f90d1e24296af69f024b3
prite36 <spritelemon36@hotmail.com>
2017-11-15 23:47:19 +0700

    test change uri part

--- messenger/send.js
+++ messenger/send.js
@@ -41,7 +41,8 @@ const selectBookingMenu = (recipientId) => {
 }
 const callSendAPI = (messageData) => {
   request({
-    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    // uri: 'https://graph.facebook.com/v2.6/me/messages',
+    uri: 'https://graph.facebook.com/v2.6/me/thread_settings',
     qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
     method: 'POST',
     json: messageData
