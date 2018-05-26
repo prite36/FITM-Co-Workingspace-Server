e1df1d2065a4534e7f8002edef3b1432485bd1a3
SPRITELEMON\Spritelemon <spritelemon36@hotmail.com>
2018-05-26 22:09:22 +0700

    change email  To co-working-space@fitm.kmutnb.ac.th

--- messenger/firebaseDB.js
+++ messenger/firebaseDB.js
@@ -11,7 +11,6 @@ var config = {
   authDomain: process.env.AUTH_DOMAIN,
   databaseURL: process.env.DATABASE_URL,
   projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
   messagingSenderId: process.env.MESSAGEING_SENDER_ID
 }
 firebase.initializeApp(config)
